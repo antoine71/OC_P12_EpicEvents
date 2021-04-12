4afb001ae3b55ae8f94eeef705b9e40086aef2ed
Antoine Rebillard <antoine.rebillard@pm.me>
2021-04-12 10:06:15 +0200

    created model

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client, Contract, Event
+
+
+admin.site.register(Client)
+admin.site.register(Contract)
+admin.site.register(Event)
--- events/models.py
+++ events/models.py
@@ -1,30 +1,51 @@
 from django.db import models
-
-# Create your models here.
-
-LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
-
-
-class Snippet(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, blank=True, default='')
-    code = models.TextField()
-    linenos = models.BooleanField(default=False)
-    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
-    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
-
-    class Meta:
-        ordering = ['created']
+from django.conf import settings
+from django.db.models.deletion import SET_NULL
 
 
 class Client(models.Model):
+    sales_contact = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
-    email = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, blank=True)
     phone = models.CharField(max_length=20, blank=True)
     mobile = models.CharField(max_length=20, blank=True)
     company_name = models.CharField(max_length=250)
+    converted = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(null=True)
+
+    class Meta:
+        ordering = ['-date_updated', '-date_created']
+
+
+class Contract(models.Model):
+    client = models.ForeignKey(
+        to=Client, on_delete=models.CASCADE)
+    signed = models.BooleanField(default=False)
+    amount = models.FloatField(null=True)
+    payment_due_date = models.DateTimeField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(null=True)
+
+    class Meta:
+        ordering = ['-date_updated', '-date_created']
+
+
+class Event(models.Model):
+    contract = models.OneToOneField(
+        to=Contract,
+        on_delete=models.CASCADE,
+        primary_key=True)
+    support_contact = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=SET_NULL, null=True)
+    attendees = models.IntegerField(null=True)
+    event_date = models.DateTimeField(null=True)
+    notes = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(null=True)
+
+    class Meta:
+        ordering = ['-date_updated', '-date_created']
