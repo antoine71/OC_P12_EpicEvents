c5f506be6091c4e2055d2dbffd2aa4fd786abcc5
Antoine Rebillard <antoine.rebillard@pm.me>
2021-04-26 21:31:22 +0200

    flake8 corrections

--- epicevents/users/models.py
+++ epicevents/users/models.py
@@ -5,8 +5,3 @@ class User(AbstractUser):
     """Default user for epicevents."""
 
     pass
-
-    # #: First and last name do not cover name patterns around the globe
-    # name = CharField(_("Name of User"), blank=True, max_length=255)
-    # first_name = None  # type: ignore
-    # last_name = None  # type: ignore
